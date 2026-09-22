0c11bf901fe35f5f8da32ffba7e6afa7a0f7f66f
dolderhe <dol25003@byui.edu>
2026-09-22 09:21:52 -0600

    Used to practice comments

---
+++ sandbox/Sandbox/classnotes.cs
@@ -0,0 +1,15 @@
+using System;
+// Comments out a line
+/*
+Comments several lines
+*/
+
+// You have to predeclare your variables example below.
+/*
+int x = 10;
+
+Console.WriteLine("text"); //
+Console.Write(); //
+
+Console.WriteLine($"x"); //
+*/
