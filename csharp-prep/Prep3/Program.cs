a31f30d12f6cc93033e33cde2c5c44b95d86d0b3
dolderhe <dol25003@byui.edu>
2026-09-24 10:17:04 -0600

    Wrote a guessing number program

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("What is the magic number? ");
+        int magic_number = int.Parse(Console.ReadLine());
+        
+        int guess;
+
+        do
+        {    
+            Console.Write("What is your guess? ");
+            string guess_str = Console.ReadLine();
+            guess = int.Parse(guess_str);
+
+            if (guess == magic_number)
+            {
+                Console.WriteLine("You guessed it!");
+            }
+            else if (guess > magic_number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < magic_number)
+            {
+                Console.WriteLine("Higher");
+            }
+            
+        } while (guess != magic_number);
+            
     }
 }
